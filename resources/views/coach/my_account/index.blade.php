22b7af3bc13da9d202df534e80641dc654d28902
captainwasi123 <captain.wasi@gmail.com>
2021-10-15 02:16:38 +0500

    14-10-2021 update

--- resources/views/coach/my_account/index.blade.php
+++ resources/views/coach/my_account/index.blade.php
@@ -368,9 +368,13 @@
                   </div>
                   <br>
                   <div class="row">
-                     <div class="col-md-12">
+                     <div class="col-md-8">
                         <label>Email</label>
                         <input type="text" name="email" value="{{Auth::user()->email}}" class="form-control" disabled>
+                     </div>
+                        <div class="col-md-4">
+                        <label>Email</label>
+                        <input type="text" name="" value="{{Auth::user()->username}}" class="form-control" disabled>
                      </div>
                   </div>
                   <br>
