22bd3be2498307254896f836dce6ae6e11cfbe62
captainwasi123 <captain.wasi@gmail.com>
2021-10-16 20:14:02 +0500

    Update 16-10-2021

--- app/Http/Controllers/coach/lessonsController.php
+++ app/Http/Controllers/coach/lessonsController.php
@@ -85,6 +85,17 @@ class lessonsController extends Controller
 
             lessons::updateImage($id, $filename);
         }
+        
+        if(!empty($request->file('media'))){
+            foreach($request->file('media') as $file)
+            {
+                $filename = date('dmyHis').'.'.$file->getClientOriginalExtension();
+                $filename = $id.'-'.$filename;
+                $mid = medias::addMedia($id, $filename);
+                $file->move(base_path('/public/storage/user/lessons/media/'), $mid.'-'.$filename); 
+            }
+        }
+
 
         return redirect()->back()->with('success', 'Lesson Updated.');
     }
