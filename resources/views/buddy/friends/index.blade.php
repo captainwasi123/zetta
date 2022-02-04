deca924b7d14960498fcaf91722327add4280274
captainwasi123 <captain.wasi@gmail.com>
2022-02-04 19:58:27 +0500

    04-02-2022

--- resources/views/buddy/friends/index.blade.php
+++ resources/views/buddy/friends/index.blade.php
@@ -80,11 +80,11 @@
                                        @foreach($requests as $val)
                                            <tr>
                                               <td class="table-image">
-                                                 <a href="{{route('web.buddy.details', base64_encode($val->user->id))}}"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{$val->user->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{$val->user->fname.' '.$val->user->lname}} <br> {{'@'.$val->user->username}} </a>
+                                                 <a href="{{route('web.buddy.details', base64_encode(@$val->user->id))}}"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{@$val->user->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{$val->user->fname.' '.$val->user->lname}} <br> {{'@'.$val->user->username}} </a>
                                               </td>
                                               <td> <i class="fa fa-star col-yellow"> </i> 4.8 </td>
                                               <td> Country: {{@$val->user->country->nicename}} </td>
-                                              <td> Since: {{date('Y', strtotime($val->user->created_at))}} </td>
+                                              <td> Since: {{date('Y', strtotime(@$val->user->created_at))}} </td>
                                               <td class="">
                                                  <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="fa fa-ellipsis-v"> </i> </a>
                                                  <div class="dropdown-menu animated flipInY" style="">
@@ -126,15 +126,15 @@
                                           @if($val->user_id == Auth::id())
                                            <tr>
                                               <td class="table-image">
-                                                 <a href="{{route('web.buddy.details', base64_encode($val->friend->id))}}" target="_blank"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{$val->friend->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{$val->friend->fname.' '.$val->friend->lname}} <br> {{'@'.$val->friend->username}} </a>
+                                                 <a href="{{route('web.buddy.details', base64_encode(@$val->friend->id))}}" target="_blank"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{@$val->friend->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{@$val->friend->fname.' '.@$val->friend->lname}} <br> {{'@'.$val->friend->username}} </a>
                                               </td>
                                               <td> <i class="fa fa-star col-yellow"> </i> 4.8 </td>
                                               <td> Country: {{@$val->friend->country->nicename}} </td>
-                                              <td> Since: {{date('Y', strtotime($val->friend->created_at))}} </td>
+                                              <td> Since: {{date('Y', strtotime(@$val->friend->created_at))}} </td>
                                               <td class="">
                                                  <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="fa fa-ellipsis-v"> </i> </a>
                                                  <div class="dropdown-menu animated flipInY" style="">
-                                                    <a href="{{URL::to('/buddy/inbox/chat/'.base64_encode($val->friend->id))}}/{{empty($val->friend->fname) ? 'New User' : $val->friend->fname.' '.$val->friend->lname}}" class="dropdown-item"> Send Message </a>
+                                                    <a href="{{URL::to('/buddy/inbox/chat/'.base64_encode($val->friend->id))}}/{{empty(@$val->friend->fname) ? 'New User' : @$val->friend->fname.' '.@$val->friend->lname}}" class="dropdown-item"> Send Message </a>
                                                     <div class="dropdown-divider"></div>
                                                     <a href="javascript:void(0)" data-id="{{base64_encode($val->id)}}" class="dropdown-item removeFriend"> Remove Friend </a>
                                                  </div>
@@ -143,15 +143,15 @@
                                           @else
                                            <tr>
                                               <td class="table-image">
-                                                 <a href="{{route('web.buddy.details', base64_encode($val->user->id))}}" target="_blank"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{$val->user->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{$val->user->fname.' '.$val->user->lname}} <br> {{'@'.$val->user->username}} </a>
+                                                 <a href="{{route('web.buddy.details', base64_encode(@$val->user->id))}}" target="_blank"><img src="{{URL::to('/')}}/public/storage/user/profile_img/{{@$val->user->profile_img}}" alt="user" onerror="this.onerror=null;this.src='{{URL::to('/')}}/assets/user_dashboard/user.png';" alt="user" width="40" class="img-circle"> {{@$val->user->fname.' '.@$val->user->lname}} <br> {{'@'.$val->user->username}} </a>
                                               </td>
                                               <td> <i class="fa fa-star col-yellow"> </i> 4.8 </td>
                                               <td> Country: {{@$val->user->country->nicename}} </td>
-                                              <td> Since: {{date('Y', strtotime($val->user->created_at))}} </td>
+                                              <td> Since: {{date('Y', strtotime(@$val->user->created_at))}} </td>
                                               <td class="">
                                                  <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="fa fa-ellipsis-v"> </i> </a>
                                                  <div class="dropdown-menu animated flipInY" style="">
-                                                    <a href="{{URL::to('/buddy/inbox/chat/'.base64_encode($val->user->id))}}/{{empty($val->user->fname) ? 'New User' : $val->user->fname.' '.$val->user->lname}}" class="dropdown-item"> Send Message </a>
+                                                    <a href="{{URL::to('/buddy/inbox/chat/'.base64_encode(@$val->user->id))}}/{{empty(@$val->user->fname) ? 'New User' : @$val->user->fname.' '.@$val->user->lname}}" class="dropdown-item"> Send Message </a>
                                                     <div class="dropdown-divider"></div>
                                                     <a href="javascript:void(0)" data-id="{{base64_encode($val->id)}}" class="dropdown-item removeFriend"> Remove Friend </a>
                                                  </div>
