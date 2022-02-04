221ed660a15b916bd35ede1987dbf2ec69bbaa42
captainwasi123 <captain.wasi@gmail.com>
2022-02-04 23:47:10 +0500

    04-02-2022

--- resources/views/buddy/activity/index.blade.php
+++ resources/views/buddy/activity/index.blade.php
@@ -205,23 +205,4 @@
       </div>
    </div>
    </div>
-
-   <script>
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn1").onclick = function() {myFunction1()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction1() {
-  document.getElementById("myDropdown1").classList.toggle("show");
-}
-</script>
-
 @endsection
